c7319fd374c522caabff5604088e6bf0a46b9e43
Kerry Zhou <51468793+KerryZhou1024@users.noreply.github.com>
2024-10-09 02:07:33 -0500

    update

--- find-duplicate/option1.cpp
+++ find-duplicate/option1.cpp
@@ -1,9 +1,10 @@
-// OPTION 1 - FIND DUPLICATE
-// DO NOT SHARE
-
 #include <cstdint>
 #include <cstdlib>
 #include <vector>
+#include <unordered_set>
+#include <iostream>
+
+
 
 using namespace std;
 
@@ -15,6 +16,31 @@ using namespace std;
 // `input` contains all integers in the domain [1, N] at least once
 // `findDuplicate` returns an `int`: the duplicate integer
 
+
+
+//this is probably the most intuitive solution. unordered set/hash table is pretty efficient
+//time O(n), as each lookup/insert is O(1) and we loop through 1-N
+//space O(n), as in the worst case visited has to store n numbers
 int findDuplicate(vector<int> input) {
 
+    unordered_set<int> visited;
+
+    for(int n : input){
+        if(visited.count(n)){
+            return n;
+        }
+
+        visited.insert(n);
+    }
+
+    return -1;
+
+}
+
+int main(){
+    //testing
+    cout << findDuplicate({1,2,3,4,5,2,6}) << endl; // outputs 2
+    cout << findDuplicate({1,2,3,5,45,7,5,4,20,111}); // outputs 5
+
+    return 0;
 }
